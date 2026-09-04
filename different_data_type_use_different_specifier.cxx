3573b8ffe8ed5b8101c94af33677b76b1f9535c0
Jayed1971 <jayedhussain398@gmail.com>
2026-09-04 16:08:01 +0600

    Add files via upload

---
+++ different_data_type_use_different_specifier.cxx
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+int Myreg = 2025331556;
+float pi = 3.1416;
+char My_first_letter_Of_name = 'J'; printf("My registration number is :%d\n",Myreg);
+printf("pi = %f\n",pi);
+printf("First letter of name : %c",My_first_letter_Of_name);
+}
