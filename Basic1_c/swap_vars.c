33620272d284acdf947e75fde9b023710b00e3c2
Jayed1971 <jayedhussain398@gmail.com>
2026-09-19 02:44:44 +0600

    Add files via upload

---
+++ Basic1_c/swap_vars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, temp;
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+   
+
+    printf("After swap: a = %d, b = %d\n", b, a);
+
+    return 0;
+}
