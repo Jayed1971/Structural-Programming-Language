e25358a29c0e4947be618688f02a7101e0905134
Jayed1971 <jayedhussain398@gmail.com>
2026-09-19 02:46:36 +0600

    Add files via upload

---
+++ Variable/var_change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int score = 50;
+    printf("Initial Score: %d\n", score);
+
+   
+    score = 75;
+    printf("Updated Score: %d\n", score);
+
+    return 0;
+}
