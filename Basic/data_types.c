2d967d2ae1e3c9314de975414e10e0be3f3122d8
Jayed1971 <jayedhussain398@gmail.com>
2026-09-19 02:37:17 +0600

    Add files via upload

---
+++ Basic/data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int id = 101;
+    float gpa = 3.75;
+    char grade = 'A';
+
+    printf("ID: %d\n", id);
+    printf("GPA: %.2f\n", gpa);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
