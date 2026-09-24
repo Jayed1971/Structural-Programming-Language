7e3e7491fe1b8043c030cd62e40a787c9a013300
Jayed1971 <jayedhussain398@gmail.com>
2026-09-25 03:30:03 +0600

    fixbug

---
+++ string /stringinput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	char s[6];
+    	s[0]='j';
+        	s[1]='a';
+            	s[2]='y';
+                	s[3]='e';
+                    	s[4]='d';
+                        	s[5]='\0';
+                            	printf("%s",s);
+                                }
+                                	
+                                    	
